fec52a4a74d77f8f505bc20bfaebf3f55687c8c8
julia-pro <juliaszatkowska52@gmail.com>
2023-11-06 12:39:28 +0100

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  
+  const bodyEl = document.body;
+  const btnStartEl = document.querySelector('[data-start]');
+  const btnStopEl = document.querySelector('[data-stop]');
+  
+  let intervalID = null;
+  
+  btnStartEl.addEventListener('click', () => {
+      toggleBTN(true)
+      intervalID = setInterval(() => {
+          bodyEl.style.background = getRandomHexColor()
+      }, 1000)
+  });
+  
+  btnStopEl.addEventListener('click', () => {
+      toggleBTN(false);
+      clearInterval(intervalID);
+  });
+  
+  function toggleBTN(bool) {
+      btnStartEl.disabled = bool;
+      btnStopEl.disabled = !bool;
+  }
